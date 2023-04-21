be36258927a0cdcf6ec136e6d1948bdfc62559cc
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-04-21 15:25:36 +0200

    practice

--- src/App.js
+++ src/App.js
@@ -1,78 +1,102 @@
-import { useState } from 'react' // state hook
-import './App.css';
-import Task from './components/Task'
-
-const initialTasks = [
- { id: 1, text: 'Go shopping', completed: false },
- { id: 2, text: 'Work out', completed: false },
- { id: 3, text: 'See the doctor', completed: true }
-]
-let id = initialTasks.length
+import { useState, useEffect } from "react"; // state hook
+import "./App.css";
+import Task from "./components/Task";
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks)
+  const [tasks, setTasks] = useState([]);
 
-  const handleSubmit = (event) => {
+  // I want to make a GET /tasks and then show this data on the page
+
+  // 1. effect hook
+  useEffect(() => {
+    fetch("http://localhost:3030/tasks")
+      // promise syntax
+      .then((res) => res.json())
+      .then((data) => setTasks([...tasks, ...data]));
+  }, []);
+  // 2. Fetch API // fetch()
+
+  const handleSubmit = async (event) => {
     // prevent default behaviour of event (in this case form submission event causes page to reload)
-    event.preventDefault()
-    id++
+    event.preventDefault();
+    // id++;
     // get value from first element within element that caused submission event
-    const text = event.target[0].value
+    const text = event.target[0].value;
     // create a new task with the correct data
     const newTask = {
-      id: id,
       text: text,
-      completed: false
-    }
-    // create new state
-    const newTasks = [...tasks, newTask]
-    // tell react to update state & rerender
-    setTasks(newTasks)
-  }
+      completed: false,
+    };
+    // NEED to make a POST HTTP request to http://localhost:3030/tasks/
+    // and then display data
 
-  const updateTasks = (taskId, value) => {
+    const res = await fetch("http://localhost:3030/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+  };
+
+  const updateTasks = async (taskId, value) => {
     // find the task
     // create a new array with the updated task
-    const updatedTasks = tasks.map(task => {
+    const res = await fetch(`http://localhost:3030/tasks/${taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed: value }),
+    });
+
+    const updatedTask = await res.json();
+
+    const updatedTasks = tasks.map((task) => {
       if (task.id === taskId) {
-        task.completed = value
+        return updatedTask;
       }
-      return task
-    })
-    setTasks(updatedTasks)
-  }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
-  const deleteTask = (taskId) => {
+  const deleteTask = async (taskId) => {
+    await fetch(`http://localhost:3030/tasks/${taskId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-    const filteredTasks = tasks.filter(item => item.id !== taskId)
+    const filteredTasks = tasks.filter((item) => item.id !== taskId);
     // find the task
     // remove it from the array of tasks
-    setTasks(filteredTasks)
-  }
+    setTasks(filteredTasks);
+  };
 
   return (
-
-    <div className='App'>
+    <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" name="task"/>
+        <input type="text" name="task" />
         <button>add task</button>
       </form>
 
-      {
-        tasks.map(item => {
-          return (
-            <Task
-              task={item}
-              key={item.id}
-              updateTasks={updateTasks}
-              deleteTask={deleteTask}
-            />
-          )})
-      }
-
+      {tasks.map((item) => {
+        return (
+          <Task
+            task={item}
+            key={item.id}
+            updateTasks={updateTasks}
+            deleteTask={deleteTask}
+          />
+        );
+      })}
     </div>
-
-  )
+  );
 }
 
 export default App;
