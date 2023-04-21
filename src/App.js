e59fe8c2e4e3cb5e919f123a0b7d6b0e5e952e24
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-04-21 18:45:02 +0300

    post, patch, delete done.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react' // state hook
 import './App.css';
 import Task from './components/Task'
 
-const initialTasks = [
- { id: 1, text: 'Go shopping', completed: false },
- { id: 2, text: 'Work out', completed: false },
- { id: 3, text: 'See the doctor', completed: true }
-]
-let id = initialTasks.length
+// const initialTasks = [
+//  { id: 1, text: 'Go shopping', completed: false },
+//  { id: 2, text: 'Work out', completed: false },
+//  { id: 3, text: 'See the doctor', completed: true }
+// ]
+// let id = initialTasks.length
 
 function App() {
   //const [tasks, setTasks] = useState(initialTasks)
@@ -29,26 +29,46 @@ function App() {
     setTask({...task, [name]: value})
   }
 
-
-  const handleSubmit = (event) => {
+  //create a task when form is submitted
+  const handleSubmit = async (event) => {
     // prevent default behaviour of event (in this case form submission event causes page to reload)
     event.preventDefault()
-    id++
-    // get value from first element within element that caused submission event
-    const text = event.target[0].value
-    // create a new task with the correct data
-    const newTask = {
-      id: id,
-      text: text,
-      completed: false
-    }
-    // create new state
-    const newTasks = [...tasks, newTask]
-    // tell react to update state & rerender
-    setTasks(newTasks)
+    //OLD WAY OF CREATING TASK
+    //id++
+    //const text = event.target[0].value
+    // const newTask = {
+    //   id: id,
+    //   text: text,
+    //   completed: false
+    // }
+    //const newTasks = [...tasks, newTask]
+    //setTasks(newTasks)
+
+    //NEW WAY FOR TASK CREATION
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks, data])
+
   }
 
-  const updateTasks = (taskId, value) => {
+  const updateTasks = async (taskId, value) => {
+
+    const res = await fetch("http://localhost:5000/tasks/${taskId}", {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({completed: value})
+    })
+    const updatedTask = await res.json()
+
+
     // find the task
     // create a new array with the updated task
     const updatedTasks = tasks.map(task => {
@@ -60,7 +80,11 @@ function App() {
     setTasks(updatedTasks)
   }
 
-  const deleteTask = (taskId) => {
+  const deleteTask = async (taskId) => {
+
+    const res = await fetch("http://localhost:5000/tasks/${taskId}",{
+      method: 'DELETE'
+    })
 
     const filteredTasks = tasks.filter(item => item.id !== taskId)
     // find the task
